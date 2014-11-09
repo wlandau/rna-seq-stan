792304c5cfd18d2c9655c43c99f4d758286ef079
wlandau <will.landau@gmail.com>
2014-11-09 14:21:08 -0600

    plotting code

--- R/plotExampleROC.R
+++ R/plotExampleROC.R
@@ -1,19 +1,38 @@
-plotExampleROC = function(file = "../roc/edgeR1-16.rds", upper = 1e-1){
-  r = readRDS(file)
-  r = r[r$fpr <= upper,]
+plotExampleROC = function(upper = 1e-1, pkgs = c("edgeR", "baySeq", "ShrinkBayes"), sample.sizes = c(4, 8, 16)){
 
-  pl = ggplot(r, aes(x = fpr, y = tpr), na.rm=TRUE) + 
-    geom_line() +
-    xlab("\nFalse positive rate (FPR)") + 
-    ylab("True positive rate (TPR)\n") +
-    theme(text = element_text(family = "Helvetica", size = 12, colour= "black", face="plain"),
-          legend.title= element_text(family = "Helvetica", size=12, face="plain"),
-          axis.text = element_text(family = "Helvetica", colour = 'black'),
-          axis.line=element_line())
+  df = NULL
+  for(pkg in pkgs){
+    for(s in sample.sizes){
+      r = readRDS(paste("../roc/", pkg, "1-", s, ".rds", sep=""))
+      r = r[r$fpr <= upper,]
+      df = rbind(df, cbind(r$fpr, r$tpr, s, pkg))
+    }
+  }
 
-  pl = pl + theme(panel.background = element_rect(fill='white'),
-                  panel.grid.major = element_line(color="lightgray"),
-                  panel.border = element_rect(color="black", fill = NA))
+  df = data.frame(df)
+  colnames(df) = c("FPR", "TPR", "SampleSize", "Method")
 
-  ggsave("../fig/exampleROC.pdf", pl, width = 5, height = 5, dpi = 1600)
+  df$FPR = as.numeric(as.vector(df$FPR))
+  df$TPR = as.numeric(as.vector(df$TPR))
+  df$Method = factor(df$Method, levels = c("edgeR", "ShrinkBayes", "baySeq"))
+  df$SampleSize = factor(df$SampleSize, levels = c(4, 8, 16))
+  df$SampleSize = revalue(df$SampleSize, c("4" = "4 samples per group", "8" = "8 samples per group", "16" = "16 samples per group"))
+
+  pl = ggplot(df, aes(x = FPR, y = TPR), na.rm=TRUE) + #+ geom_line() +
+         geom_line(aes(color = Method)) + 
+         xlab("\nFalse positive rate (FPR)") + 
+         ylab("True positive rate (TPR)\n") +
+         facet_grid(~ SampleSize) + 
+         theme(axis.text = element_text(family = "Helvetica", color = 'black'),
+                    axis.text.x = element_text(angle = -45, hjust = 0),
+                    panel.background = element_rect(fill='white'),
+                    panel.border = element_rect(color="black", fill = NA),
+                    panel.grid.major = element_line(color="lightgray"),
+                    text = element_text(family = "Helvetica", colour= "black"))
+
+  ggsave("../fig/exampleROC.pdf", pl, width = 8, height = 5, dpi = 1600)
+
+  pl = pl + theme(legend.position="none")
+
+  ggsave("../fig/exampleROC-nolegend.pdf", pl, width = 8, height = 5, dpi = 1600)
 }
