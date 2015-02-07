15d4090fc08951de371c2d506c01add61714bbe9
wlandau <will.landau@gmail.com>
2015-02-07 15:07:34 -0600

    unpacked some posterior probabilities

--- R/utils.R
+++ R/utils.R
@@ -1,5 +1,5 @@
 work.parms = function(item){
-  mtd = c("edgeR", "baySeq", "stan_corr", "ShrinkBayes", "stan", "stan_laplace", "stan_laplace_mvn", "ShrinkBayesMu")
+  mtd = c("edgeR", "baySeq", "stan_corr", "ShrinkBayes", "stan", "stan_laplace", "stan_laplace_mvn", "ShrinkBayesMu", "pprob_lap_phimix", "rev_probs_laplace", "rev_probs_mu_cov", "revised_pvals")
   switch(item, 
              "mtd" = ordered(mtd, levels = mtd),
              "size" = c(4, 8, 16),
@@ -17,7 +17,7 @@ loopify = function(fun, mtd = work.parms("mtd"), size = work.parms("size"), rep 
   as.data.frame(t(Vectorize(fun, c("mtd", "size", "rep"))(.grid$mtd, .grid$size, .grid$rep)))
 }
 
-unpack.stan = function(mtds = c("stan_corr", "stan", "stan_laplace", "stan_laplace_mvn")){
+unpack.stan = function(mtds){
   for(mtd in intersect(mtds, c("stan_corr", "stan", "stan_laplace"))){
     file = switch(mtd,
                         "stan_corr" = "../ranks/stan_probs_corr.rds",
@@ -36,6 +36,28 @@ unpack.stan = function(mtds = c("stan_corr", "stan", "stan_laplace", "stan_lapla
     })
   }
 
+  for(mtd in intersect(mtds, c("rev_probs_laplace", "rev_probs_mu_cov", "revised_pvals"))){
+    file = switch(mtd,
+                        "rev_probs_laplace" = "../other_Eric_posterior_probs/rev_probs_laplace.rds", 
+                        "rev_probs_mu_cov" = "../other_Eric_posterior_probs/rev_probs_mu_cov.rds", 
+                        "revised_pvals" = "../other_Eric_posterior_probs/revised_pvals.rds")
+
+    all.probs = readRDS(file)
+    if(mtd == "revised_pvals")
+      all.probs$new_prob = all.probs$V1
+
+    ddply(all.probs, .variables = .(sample.size, sim), .fun = function(x){
+
+#      probs = 1 - pmax(x$phph, x$plph)
+#      probs = 1 - (x$phph + x$plph)
+    probs = 1 - x$new_prob
+     
+      names(probs) = x$geneid
+      saveRDS(probs, paste("../ranks/", file.name(mtd, x$sample.size[1], x$sim[1]), sep=""))
+    })
+  }
+
+
   if(mtd == "stan_laplace_mvn"){
     file.begin = "../laplace_mvn_posterior_probs/pprob_lap_mvn"
     for(size in work.parms("size")){
@@ -44,6 +66,18 @@ unpack.stan = function(mtds = c("stan_corr", "stan", "stan_laplace", "stan_lapla
 #       probs = 1 - pmax(x$phph, x$plph)
         probs = 1 - (x$phph + x$plph)
         saveRDS(probs, paste("../ranks/", file.name("stan_laplace_mvn", size, rep), sep=""))
+      }
+    }
+  }
+
+  if(mtd == "pprob_lap_phimix"){
+    file.begin = "../laplace_mvn_posterior_probs/pprob_lap_phimix"
+    for(size in work.parms("size")){
+      for(rep in work.parms("rep")){
+        x = readRDS(paste(file.begin, size, "-", rep, sep=""))
+#       probs = 1 - pmax(x$phph, x$plph)
+        probs = 1 - (x$phph + x$plph)
+        saveRDS(probs, paste("../ranks/", file.name("pprob_lap_phimix", size, rep), sep=""))
       }
     }
   }
