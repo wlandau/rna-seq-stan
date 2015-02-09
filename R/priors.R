4e15f50a9260adb963d3716cad5351a56690318c
wlandau <will.landau@gmail.com>
2015-02-09 13:03:08 -0600

    priors

--- R/priors.R
+++ R/priors.R
@@ -1,7 +1,7 @@
 collectShrinkBayesMu = function(){
 d = NULL
 for(size in c(4, 8, 16)) for(rep in 1:10){
-  file = paste("../priors/", mtd, "-", size, "-", rep, ".rds", sep="")
+  file = paste("../priors/ShrinkBayesMu-", size, "-", rep, ".rds", sep="")
   if(file.exists(file)){
     x = readRDS(file)
     d = rbind(d, c(
@@ -10,9 +10,11 @@ for(size in c(4, 8, 16)) for(rep in 1:10){
       mu.hybrid = x$pmlist$mufixed,
       mu.parent1 = x$pmlist$mumu.parent1,
       mu.parent2 = x$pmlist$mumu.parent2,
+      mu.dispersion = x$pmlist$mudisp,
       prec.hybrid = x$pmlist$precfixed,
       prec.parent1 = x$pmlist$precmu.parent1,
-      prec.parent2 = x$pmlist$precmu.parent2
+      prec.parent2 = x$pmlist$precmu.parent2,
+      prec.dispersion = x$pmlist$precdisp
     ))
   }
 }
