7392f8202ea0e8631f93fa68ea378d7c60de5c1a
wlandau <will.landau@gmail.com>
2015-02-10 12:48:34 -0600

    priors

--- R/priors.R
+++ R/priors.R
@@ -1,3 +1,26 @@
+collectShrinkBayes = function(){
+d = NULL
+for(size in c(4, 8, 16)) for(rep in 1:10){
+  file = paste("../priors/ShrinkBayes-", size, "-", rep, ".rds", sep="")
+  if(file.exists(file)){
+    x = readRDS(file)
+    d = rbind(d, c(
+      size = size,
+      rep = rep,
+      mean.phi = x$pmlist$mufixed,
+      mean.alpha = x$pmlist$mualpha,
+      mean.delta = x$pmlist$mudelta,
+      mean.dispersion = x$pmlist$mudisp,
+      prec.phi = x$pmlist$precfixed,
+      prec.alpha = x$pmlist$precmu.alpha,
+      prec.delta = x$pmlist$precmu.delta,
+      prec.dispersion = x$pmlist$precdisp
+    ))
+  }
+}
+  saveRDS(as.data.frame(d), "../priors/priors-ShrinkBayes.rds")
+}
+
 collectShrinkBayesMu = function(){
 d = NULL
 for(size in c(4, 8, 16)) for(rep in 1:10){
@@ -7,13 +30,13 @@ for(size in c(4, 8, 16)) for(rep in 1:10){
     d = rbind(d, c(
       size = size,
       rep = rep,
-      mu.hybrid = x$pmlist$mufixed,
-      mu.parent1 = x$pmlist$mumu.parent1,
-      mu.parent2 = x$pmlist$mumu.parent2,
+      mu.phi = x$pmlist$mufixed,
+      mu.alpha = x$pmlist$mumu.alpha,
+      mu.delta = x$pmlist$mumu.delta,
       mu.dispersion = x$pmlist$mudisp,
-      prec.hybrid = x$pmlist$precfixed,
-      prec.parent1 = x$pmlist$precmu.parent1,
-      prec.parent2 = x$pmlist$precmu.parent2,
+      prec.phi = x$pmlist$precfixed,
+      prec.alpha = x$pmlist$precmu.alpha,
+      prec.delta = x$pmlist$precmu.delta,
       prec.dispersion = x$pmlist$precdisp
     ))
   }
